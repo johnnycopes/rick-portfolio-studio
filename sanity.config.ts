675557da0acbbd1fef265ada6c15b408e2e0bbed
John Coppola <jdcoppola@gmail.com>
2026-03-20 10:29:11 -0400

    CONFIG: updates sanity.config to use ordering plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,16 +1,32 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
+import {orderableDocumentListDeskItem} from '@sanity/orderable-document-list'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
-  title: 'Rick\'s Portfolio',
+  title: "Rick's Portfolio",
 
   projectId: 'hzn4ro6m',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool({
+      structure: (S, context) =>
+        S.list()
+          .title('Content')
+          .items([
+            orderableDocumentListDeskItem({
+              type: 'workProject',
+              title: 'Work Projects',
+              S,
+              context,
+            }),
+          ]),
+    }),
+    visionTool(),
+  ],
 
   schema: {
     types: schemaTypes,
