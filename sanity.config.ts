4ee3b783de3fa31b9bd3aab0631d1ba8ad228c3f
John Coppola <jdcoppola@gmail.com>
2026-03-20 11:01:16 -0400

    FEATURE: adds multiple views for the Work content

--- sanity.config.ts
+++ sanity.config.ts
@@ -17,12 +17,61 @@ export default defineConfig({
         S.list()
           .title('Content')
           .items([
-            orderableDocumentListDeskItem({
-              type: 'workProject',
-              title: 'Work Projects',
-              S,
-              context,
-            }),
+            S.listItem()
+              .title('Work Projects')
+              .child(
+                S.list()
+                  .title('Work Projects')
+                  .items([
+                    // Drag-and-drop ordering
+                    orderableDocumentListDeskItem({
+                      type: 'workProject',
+                      title: 'Ordered',
+                      S,
+                      context,
+                    }),
+
+                    S.divider(),
+
+                    // Alphabetical by Title
+                    S.listItem()
+                      .title('By Title')
+                      .child(
+                        S.documentList()
+                          .title('By Title')
+                          .schemaType('workProject')
+                          .apiVersion('2024-01-01')
+                          .filter('_type == "workProject"')
+                          .defaultOrdering([{field: 'title', direction: 'asc'}]),
+                      ),
+
+                    // Grouped by Client
+                    S.listItem()
+                      .title('By Client')
+                      .child(async () => {
+                        const clients = await context
+                          .getClient({apiVersion: '2024-01-01'})
+                          .fetch(`array::unique(*[_type == "workProject"].client) | order(@ asc)`)
+
+                        return S.list()
+                          .title('Clients')
+                          .items(
+                            clients.map((client: string) =>
+                              S.listItem()
+                                .title(client)
+                                .child(
+                                  S.documentList()
+                                    .title(client)
+                                    .schemaType('workProject')
+                                    .apiVersion('2024-01-01')
+                                    .filter('_type == "workProject" && client == $client')
+                                    .params({client}),
+                                ),
+                            ),
+                          )
+                      }),
+                  ]),
+              ),
           ]),
     }),
     visionTool(),
