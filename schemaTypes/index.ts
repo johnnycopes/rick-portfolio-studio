05207af3d5d8ef2621e8a22aaf05b8fae6020e5b
John Coppola <jdcoppola@gmail.com>
2026-03-20 09:59:15 -0400

    FILE: adds index.ts and workProject.ts to the schemaTypes folder

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import { workProject } from "./workProject";
+
+// Add all document types to this array.
+// As you build out the About and Misc sections,
+// you'll add their schemas here too.
+export const schemaTypes = [workProject];
